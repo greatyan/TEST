8aa753e2ccac6be4371f45c4cad62e4710ed5eb7
greatyan <greatyan@gmail.com>
2011-07-21 11:03:02 -0700

    My first application to GIT

---
+++ test/TestProject/src/gmail/greatyan/TestMain.java
@@ -0,0 +1,14 @@
+package gmail.greatyan;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("This is my first application");
+
+	}
+
+}
