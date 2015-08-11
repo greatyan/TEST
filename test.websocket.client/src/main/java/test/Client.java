e6c4f0119c2ba42f2fe420b6aa9b5a060ae32721
Wei Yan <greatyan@gmail.com>
2015-08-10 23:14:35 -0700

    first check in

---
+++ test.websocket.client/src/main/java/test/Client.java
@@ -0,0 +1,5 @@
+import test;
+
+public class Client 
+{
+
---
+++ test.websocket/src/main/java/test/websocket/client/Main.java
@@ -0,0 +1,8 @@
+package test.websocket.client;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("OK");
+    }
+}
